69193f64968395e87ba80cb3760877a6d12e356a
Marianita13 <marianitacero.1913@gmail.com>
2023-04-20 09:46:00 -0500

    buttons

--- main.js
+++ main.js
@@ -3,55 +3,80 @@ const numero2 = document.getElementById('num2')
 const operando = document.getElementById('operando')
 const resultado = document.getElementById('display')
 const botones = document.querySelector('#botones')
-let num1=0
+let num1=''
+let numeros=[]
 
 botones.addEventListener('click', (evento)=>{
+    if (evento.target.id=='btn#0'){
+        num1=0
+        resultado.textContent+=num1
+        num1=''
+    }
+    if (evento.target.id=='btn#1'){
+        num1=1
+        resultado.textContent+=num1
+        num1=''
+    }
+    if (evento.target.id=='btn#2'){
+        num1=2
+        resultado.textContent+=num1
+        num1=''
+    }
+    if (evento.target.id=='btn#3'){
+        num1=3
+        resultado.textContent+=num1
+        num1=''
+    }
+    if (evento.target.id=='btn#4'){
+        num1=4
+        resultado.textContent+=num1
+        num1=''
+    }
+    if (evento.target.id=='btn#5'){
+        num1=5
+        resultado.textContent+=num1
+        num1=''
+    }
+    if (evento.target.id=='btn#6'){
+        num1=6
+        resultado.textContent+=num1
+        num1=''
+    }
+    if (evento.target.id=='btn#7'){
+        num1=7
+        resultado.textContent+=num1
+        num1=''
+    }
+    if (evento.target.id=='btn#8'){
+        num1=8
+        resultado.textContent+=num1
+        num1=''
+    }
+    if (evento.target.id=='btn#9'){
+        num1=9
+        resultado.textContent+=num1
+        num1=''
+    }
+
+
     if(evento.target.id=='suma'){
-        operando.innerHTML='+'
-        num1=numero1.value
-        numero1.value=''
+        resultado.textContent+='+'
     }
     if(evento.target.id=='rest'){
-        operando.innerHTML='-'
-        num1=numero1.value
-        numero1.value=''
+        resultado.textContent+='-'
     }
     if (evento.target.id=='divi'){
-        operando.innerHTML='/'
-        num1=numero1.value
-        numero1.value=''
+        resultado.textContent+='/'
     }
     if (evento.target.id=='mult'){
-        operando.innerHTML='*'
-        num1=numero1.value
-        numero1.value=''
+        resultado.textContent+='*'
     }
+
     if(evento.target.id=='igual'){
-        if (operando.innerHTML=='+'){
-            let suma=+num1 + (+numero1.value)
-            resultado.innerHTML=suma
-            numero1.value=''
-        }else if (operando.innerHTML=='-'){
-            let resta=num1 - numero1.value
-            resultado.innerHTML=resta
-            numero1.value=''
-        }else if (operando.innerHTML=='/'){
-            let divi=num1 / numero1.value
-            resultado.innerHTML=divi
-            numero1.value=''
-        }else if (operando.innerHTML=='*'){
-            let mult=num1 * numero1.value
-            resultado.innerHTML=mult
-            numero1.value=''
-        }else{
-            alert('Selecciona una operación')
-        }
+
     }
     if (evento.target.id=='limpiar'){
         resultado.innerHTML=''
-        operando.innerHTML=''
-        numero1.value=''
-        numero2.value=''
     }
 })
 
